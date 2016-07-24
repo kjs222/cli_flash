938c483d52f97d79f46c115c9775efef43f9352a
Kerry <kerry.sheldon@gmail.com>
2016-07-24 10:27:55 -0600

    uses figaro, but getting wierd output at beginning

--- quizlet.rb
+++ quizlet.rb
@@ -2,16 +2,20 @@ require 'thor'
 require 'faraday'
 require 'json'
 require 'figaro'
-# APIID = 'ACPSE89AY8'
-# APIKEY = 'qfzpgN94Ag3gayPYv3dyjP'
+require 'pry'
 
+ENV['FIGARO_ENVIRONMENT'] = "#{ENV['RACK_ENV']}" || 'development'
+
+Figaro.application = Figaro::Application.new(environment: ENV['FIGARO_ENVIRONMENT'], path: 'application.yml')
+
+Figaro.load
 
 class QuizletCLI < Thor
 
   desc "quiz_sets", "Returns the titles for all of your quiz sets"
     def quiz_sets
       connection = Faraday.new('https://api.quizlet.com')
-      connection.headers["Authorization"] = "Bearer 4vGGwud3seTc2WxJEbsV72YMcKzZwymYbGPMKERp"
+      connection.headers["Authorization"] = "Bearer " + ENV['QUIZLET_TOKEN']
       data = connection.get("/2.0/users/kjs222/sets")
       parsed_data = JSON.parse(data.body)
       parsed_data.each do |quiz_set|
